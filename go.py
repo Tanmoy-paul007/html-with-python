4bbe25cc9df74d1c424a2dabe6fdcb03dfec9771
Tanmoy-Paul <tanmoypaul2345@gmail.com>
2025-03-21 11:49:00 +0600

    Add if ealse conditions

--- go.py
+++ go.py
@@ -2,3 +2,16 @@ print("I am Tanmoy Paul")
 
 n=int(input("Enter Your Age"))
 print(n)
+
+a = int(input("Enter your age: "))
+
+# If else statement
+if(a>=18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+else:
+    print("You are below the age of consent")
+
+
+print("End of Program")
